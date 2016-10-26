cf46f685ebdeed16d7cc9573e3e36e1928340a31
André Krebs <andre.krebssilva@gmail.com>
2016-10-25 22:45:57 -0200

    Metodo configure sobrescrito para encode de senha

--- src/main/java/eti/krebscode/config/SecurityConfig.java
+++ src/main/java/eti/krebscode/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package eti.krebscode.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService users;
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 				.authenticated()
 			.and().logout().logoutSuccessUrl("/").permitAll();*/
+	}
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(users).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 }
